62eefe0a5cc4ebe57f9973fd4dd95145f8610701
dimple06varshney <dimplevarshney06@gmail.com>
2021-12-17 02:21:48 +0530

    products1 page done

---
+++ products1.js
@@ -0,0 +1,110 @@
+import showProducts1 from "./scripts/showproducts1.js";
+
+let dVproducts1_cont = document.getElementById("dVproducts1_cont");
+let localData = JSON.parse(localStorage.getItem("makeup")) || []
+async function products()
+{
+    try{
+    // let url = `http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline`;
+    // let res = await fetch(url);
+    // let data = await res.json();
+    // console.log(data);
+    // localStorage.setItem("makeup", JSON.stringify(data))
+    showProducts1(localData,dVproducts1_cont)
+    }
+    catch(err){
+        console.log(`err: `,err);
+    }
+}
+products()
+
+let brands = document.getElementById("dVbrands");
+brands.addEventListener("click",showbrands);
+let countBrand = 0;
+function showbrands()
+{
+    countBrand++;
+   let brandName= document.getElementById("dVshowBrand");
+    if(countBrand%2===1)
+   {  
+            brandName.style.display="none";
+            // console.log(`open`, count);
+    }
+    else{
+         brandName.style.display="block"; 
+        // console.log(`close`,count);
+    }   
+}
+
+let dVbrand = document.getElementsByClassName("dVbrand");
+for(let k=0;k<dVbrand.length; k++)
+{
+    //console.log(dVbrand[k].textContent);
+    dVbrand[k].addEventListener("click",showCategory)
+    dVbrand[k].myParam =  dVbrand[k].textContent
+}
+let selectCateg = `maybelline`;
+ async function showCategory(evt){
+   
+    try{
+   selectCateg = evt.currentTarget.myParam;
+   let categurl = `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${selectCateg}`
+   let res = await fetch(categurl);
+   let categData = await res.json();
+   dVproducts1_cont.innerHTML = null;
+   if(categData.length>0)
+   showProducts1(categData,dVproducts1_cont)
+    else
+    dVproducts1_cont.innerHTML = '<center><h1>No Product Found</h1></center>';
+    }
+    catch(err){
+        console.log(err);
+        dVproducts1_cont.innerHTML = '<h1>No Product Found</h1>';
+    }
+   
+}
+
+let dVtypes = document.getElementById("dVtype");
+dVtypes.addEventListener("click",showType);
+let count_tpye = 0;
+function showType()
+{
+    count_tpye++;
+   let dVshowType= document.getElementById("dVshowType");
+    if(count_tpye%2===1)
+   {  
+       dVshowType.style.display="none";
+            // console.log(`open`, count);
+    }
+    else{
+        dVshowType.style.display="block"; 
+        // console.log(`close`,count);
+    }   
+}
+
+let dVtype = document.getElementsByClassName("dVtype");
+for(let k=0;k<dVtype.length; k++)
+{
+    //console.log(dVbrand[k].textContent);
+    dVtype[k].addEventListener("click",showTypes)
+    dVtype[k].myParam =  dVtype[k].textContent
+}
+ async function showTypes(evt){
+   
+    try{
+   let selectType = evt.currentTarget.myParam;
+   let categurl = `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${selectCateg}&product_type=${selectType}`
+   let res = await fetch(categurl);
+   let categData = await res.json();
+   dVproducts1_cont.innerHTML = null;
+   if(categData.length>0)
+   showProducts1(categData,dVproducts1_cont)
+   else
+   dVproducts1_cont.innerHTML = '<h1>No Product Found</h1>';
+    }
+    catch(err){
+        console.log(err);
+        dVproducts1_cont.innerHTML = '<h1>No Product Found</h1>';
+    }
+   
+}
---
+++ scripts/showproducts1.js
@@ -0,0 +1,57 @@
+async function showProducts1(data, location)
+{
+   console.log(data);
+   let i=0;
+    data.forEach(el => {
+            if(i <= 28)
+            {
+            //console.log(el);
+            let makeupDiv = document.createElement("div");
+            let img = document.createElement("img");
+            img.src = el.image_link;
+            let name = document.createElement("p");
+            name.innerText = el.name;
+            let price = document.createElement("p");
+            let priceValue = el.price;
+            price.innerText = `${priceValue} $`;
+            let starsDiv = document.createElement("div");
+            starsDiv.setAttribute("id","starsDiv")
+            for(let st = 0;st<=5;st++)
+            {
+                let star = document.createElement("div");
+                star.innerHTML = '<span class="star-wrap"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 17.42L6.25 21.54C6.1816 21.5921 6.09823 21.6207 6.01227 21.6217C5.9263 21.6227 5.8423 21.596 5.77272 21.5455C5.70314 21.495 5.65169 21.4234 5.62598 21.3413C5.60027 21.2593 5.60168 21.1712 5.63 21.09L7.77 14.35L2.08 10.15C2.01515 10.0984 1.96747 10.0283 1.94325 9.94905C1.91902 9.86979 1.91938 9.78505 1.94429 9.706C1.9692 9.62695 2.01747 9.5573 2.08276 9.50624C2.14805 9.45518 2.22728 9.42511 2.31 9.41999L9.38 9.36999L11.62 2.65999C11.6463 2.57996 11.6972 2.51028 11.7654 2.46088C11.8337 2.41148 11.9158 2.38489 12 2.38489C12.0842 2.38489 12.1663 2.41148 12.2346 2.46088C12.3028 2.51028 12.3537 2.57996 12.38 2.65999L14.62 9.36999L21.69 9.41999C21.7727 9.42511 21.852 9.45518 21.9172 9.50624C21.9825 9.5573 22.0308 9.62695 22.0557 9.706C22.0806 9.78505 22.081 9.86979 22.0568 9.94905C22.0325 10.0283 21.9848 10.0984 21.92 10.15L16.23 14.35L18.37 21.09C18.3983 21.1712 18.3997 21.2593 18.374 21.3413C18.3483 21.4234 18.2969 21.495 18.2273 21.5455C18.1577 21.596 18.0737 21.6227 17.9877 21.6217C17.9018 21.6207 17.8184 21.5921 17.75 21.54L12 17.42Z" fill="black"></path></svg></span>'
+                starsDiv.append(star)
+            }
+            let shades = document.createElement("div");
+            shades.setAttribute("id","shades")
+            let shadeArr= el.product_colors;
+            if(shadeArr.length>0)
+            {
+                for(let sh =0; sh < shadeArr.length;sh++)
+                {
+                    let shade = document.createElement("div");
+                    shade.style.backgroundColor = shadeArr[sh].hex_value
+                    // console.log(shadeArr[sh].hex_value,`shade`);
+                    shades.append(shade)
+                }
+            }
+            
+            let wish_buyDiv = document.createElement("div");
+            wish_buyDiv.setAttribute("id","wish_buyDiv")
+            let heart = document.createElement("button");
+            heart.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 23 20"><title>Heart icon</title><path class="heart-icon-path" fill="#fff" stroke="currentColor" d="M11.4967297,19.0021565 C12.1501607,18.4744665 15.7313591,16.1461023 16.6556949,15.4660553 C20.4639993,12.6642314 22.5,9.83806845 22.500204,6.31427989 C22.4080534,3.08900922 19.7336922,0.5 16.5,0.5 C14.6798666,0.5 13.0132876,1.30878098 11.8904344,2.71234752 L11.5,3.20039053 L11.1095656,2.71234752 C9.98671236,1.30878098 8.32013337,0.5 6.5,0.5 C3.16873226,0.5 0.5,3.08355995 0.5,6.3 C0.5,9.87466924 2.55294628,12.7216506 6.38828771,15.5301224 C7.34346545,16.229562 10.7334347,18.4195137 11.4967297,19.0021565 Z"></path></svg>'
+            
+            heart.setAttribute("id","dVheart")
+            let buyBtn = document.createElement("button")
+            buyBtn.innerText = `Buy Now`;
+            buyBtn.setAttribute("id","dVbuyBtn");
+            wish_buyDiv.append(heart,buyBtn)
+
+            makeupDiv.append(img,name,price,starsDiv,shades,wish_buyDiv)
+            location.append(makeupDiv)
+            i++; 
+          } 
+    });
+}
+
+export default showProducts1
