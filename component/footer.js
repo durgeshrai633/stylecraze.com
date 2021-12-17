da21c7d184ad17f729ba3a21f69368ec74a7f6fa
dhuruvkarn <kumardeepak971857@gmail.com>
2021-12-17 11:03:35 +0530

    footer,home,payment

---
+++ component/footer.js
@@ -0,0 +1,39 @@
+function footer() {
+    return `  <div id = "DK_cont">
+    <div id = "DK_flex">
+        <div class = "DK_about">
+            <a href="">About Us</a>
+            <a href="">Image Usage Policy</a>
+            <a href="">Contact Us</a>
+            <a href="">Editorial Guidelines</a>
+            <a href="">Join Our Medical Board</a>
+            <a href="">Press Room</a>
+
+        </div>
+        <div class = "DK_about">
+            <a href="">Advertise With Us</a>
+            <a href="">Privacy Policy</a>
+            <a href="">Terms Of Use</a>
+            <a href="">Affiliate Disclosure</a>
+            <a href="">Cookie Policy</a>
+        </div>
+    </div>
+    <div id = "DK_follow">
+        <div id = "fDK_follow_text">Follow us</div>
+        <a href=""><i class="fab fa-facebook-square"></i></a>
+        <a href=""><i class="fab fa-pinterest-square"></i></a>
+        <a href=""><i class="fab fa-twitter-square"></i></a>
+        <a href=""><i class="fab fa-instagram-square"></i></a>
+        <a href=""><i class="fab fa-youtube"></i></a>
+    </div>
+    <div id = "DK_sister">
+        <div id = "DK_oursister">Our Sister Sites</div>
+        <a href="">Mom Junction</a>
+        <a href="">The Bridal Box</a>
+        <a href="">Skinkraft</a>
+        <a href="">Vedix</a>
+    </div>
+</div>`;
+}
+
+export default footer;
